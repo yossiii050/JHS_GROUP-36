9fd2c1ba960ed79f286f52543150578d99e58ea2
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2022-12-10 19:23:17 +0200

    update candidate

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse # for recieve http response
 
-# Create your views here.
+def reg(request):
+    return HttpResponse("This is a register page.")
