5ce74f9440f457b8d3589aedae71237e19deba3c
idotamsis <idotamsis@gmail.com>
2022-12-15 18:33:28 +0200

    added path for the ticket system outside the admin, works good except the sumbit button, will be fixed in the next commit.

--- tech/models.py
+++ tech/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class ticket(models.Model):
-    title = models.CharField(("Sumbit your ticket"), max_length=50)
+    title = models.CharField(("Main claim"), max_length=50)
     body = models.TextField()
     #date = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
            return self.title
+
 
--- tech/urls.py
+++ tech/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.contrib import admin  
 from tech import views
 
 urlpatterns = [
     path('ticket/',views.ticket),
+   # path('index/', views.index),  
 ]
