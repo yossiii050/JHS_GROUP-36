4793295965b5adf79dc93e443aee6f71bb4c63f3
gabrielbenshalom <117106219+gabrielbenshalom@users.noreply.github.com>
2022-12-15 16:13:51 +0200

    update home page

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('', views.home_page_view, name='home page site'),
+    path('', views.home_page_view, name='home_page'),
     #path('', views.home_template, name='home page site'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
-def home_page_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_page_view(request,*args, **kwargs):
+    return render(request,"home.html",{})
 
 #def home_template(request):
  #   context={}
