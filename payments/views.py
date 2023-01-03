740c456bf0cebd8b4458a01200e839256c7d86c2
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2023-01-03 22:51:53 +0200

    reports for vip members-add group of vip members how pay

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User, Group
+
+def add_user_to_vip_group(request):
+  if request.method == 'POST':
+    email = request.POST['email']
+    user = User.objects.get(email=email)
+    vip_group = Group.objects.get(name='VIP')
+    vip_group.user_set.add(user)
+  return render(request, 'home.html')
+
+
 def paymentpage(request):
     return render(request,'paymentview.html')
+
