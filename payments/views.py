3768f825bc676bd555dc7c17c20f8c316f50e3da
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2023-01-05 17:54:11 +0200

    new candidate profile that be a diffrent from employer profile

--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User, Group
 
 def add_user_to_vip_group(request):
@@ -9,6 +9,11 @@ def add_user_to_vip_group(request):
     user.groups.add(group)
   return render(request, 'home.html')
 
+def ifEmployer(request):
+  for group in request.user.groups.all:
+    if group.name == 'Employers':
+      return True
+  return False    
 
 def paymentpage(request):
     user= request.user 
