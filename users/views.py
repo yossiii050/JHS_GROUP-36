5ab159a174c2e430bff2e7e903886a97db820dbd
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2023-01-07 17:39:29 +0200

    admin can make delete and edit profiles

--- users/views.py
+++ users/views.py
@@ -167,9 +167,6 @@ def view_groups(request):
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
     print(user)
-    #print(user.employer.is_employer)
-    if request.user.username != user.username:
-        return redirect('/')
     try:
         if user.employer.is_employer==True:
             employer = user.employer
