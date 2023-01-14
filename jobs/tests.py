1a03d3a818128db75e644265af9002fa54bb3fa9
gabrielbenshalom <117106219+gabrielbenshalom@users.noreply.github.com>
2023-01-14 16:09:58 +0200

    unit test23

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,19 +1,48 @@
 
-
-from django.test import TestCase
-from .models import Upload
+from django.contrib.auth.models import User
+from django.test import TestCase,RequestFactory
+from .models import Upload, Candidate
 from .forms import UploadForm
+from users.models import Employer
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from jobs.views import applyCv
 
-class UploadTestCase(TestCase):
+#class UploadTestCase(TestCase):
+#    def setUp(self):
+#        # Create an instance of the Upload model
+#        self.upload = Upload.objects.create(
+#            title='Test Title',
+#            subTitle='Test Subtitle',
+#            body='Test body',
+#        )
+
+#    def test_upload_str(self):
+#        self.assertEqual(self.upload.title, 'Test Title')
+#        self.assertEqual(self.upload.subTitle, 'Test Subtitle')
+#        self.assertEqual(self.upload.body, 'Test body')
+
+class ApplyCvTest(TestCase):
+
     def setUp(self):
-        # Create an instance of the Upload model
-        self.upload = Upload.objects.create(
-            title='Test Title',
-            subTitle='Test Subtitle',
-            body='Test body',
+        # Create a user, a job and a candidate
+        self.factory = RequestFactory()
+        self.job = Upload.objects.create(title='Test Job', slug='test-job')
+        self.user = User.objects.create_user(
+            username='candidate1', email='candidate1@example.com', password='testpass'
         )
-
-    def test_upload_str(self):
-        self.assertEqual(self.upload.title, 'Test Title')
-        self.assertEqual(self.upload.subTitle, 'Test Subtitle')
-        self.assertEqual(self.upload.body, 'Test body')
+        self.candidate = Candidate.objects.create(
+            user=self.user,
+            email='candidate1@example.com',
+            username='candidate1',
+            candidate_id='12345',
+            date_of_birth='2000-01-01',
+            phone_number='123-456-7890',
+            first_name='Test',
+            last_name='Candidate',
+            is_candidate=True,
+            bios='Test candidate bio'
+        )
+    def test_applyCv(self):
+        request = self.factory.post(reverse('jobs:applyCv', kwargs={'upload_id': self.job.slug}), {'username': self.user.username})
+        request.user = self.user
--- payments/tests.py
+++ payments/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User, Group
+from django.urls import reverse
 
-# Create your tests here.
+
+class AddUserToVipGroupTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_paymentpage(self):
+        # Make a GET request to the view
+        response = self.client.get('/paymentpage/')
+        
+        
+        # Assert that the user is now in the VIP group
+        vip_group = Group.objects.get(name='VIP')
+        self.assertIn(self.user, vip_group.user_set.all())
+        
+        # Assert that the correct template was used
+        self.assertTemplateUsed(response, 'paymentview.html')
+        
+    def test_paymentpage_group_does_not_exist(self):
+        # Delete the VIP group
+        Group.objects.filter(name='VIP').delete()
+        
+
+'''
+
+class DonateTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        #group = Group.objects.create(name='VIP')
+        #group.save()
+
+    def test_donate_page(self):
+        response = self.client.get('/paymentspage/')
+        #response = self.client.get(reverse('paymentpage'))
+        
+        # Check that the response has a status code of 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the rendered HTML contains the expected text
+        self.assertContains(response, 'DONATE')
+        self.assertContains(response, 'amount: { value: 88.44')
+    '''
