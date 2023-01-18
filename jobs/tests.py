373919cca365d7d3f217eee8469d171b3d29c622
neriala <neria616@gmail.com>
2023-01-19 00:30:55 +0200

    prep for the pre

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase,RequestFactory
 from django.urls import reverse
 from users.models import Candidate, Employer
-from jobs.forms import UploadForm
+from jobs.forms import UploadForm,SortForm
+from jobs.views import Upload_list, uploadJob
+import jobs.urls
 from django.contrib.auth.models import User
 from jobs.models import Upload
 from . import views
 
-"""class UploadJobTest(TestCase):
-#USER STORY 1
+class UploadJobTest(TestCase):
     def setUp(self):
         user = User.objects.create_user(
             username='employer1', email='employer1@example.com', password='testpass'
@@ -36,10 +37,6 @@ from . import views
             'availableAmount': 5,
             'notification': 5,
         }
-
-    def test_uploadJob_redirects_to_success(self):
-        response = self.client.post(reverse('upload'), self.form_data)
-        self.assertRedirects(response, reverse('success'))
 
 #Test that the view returns a success template when a valid form is submitted:
     def test_upload_job_success(self):
@@ -77,4 +74,48 @@ from . import views
         job = Upload.objects.filter(title='Test Job')
         self.assertTrue(job.exists())
 
-"""   
+class UploadListViewTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='employer1', email='employer1@example.com', password='testpass')
+        self.employer = Employer.objects.create(
+            user=user,
+            email='employer1@example.com',
+            username='employer1',
+            CompanyName='Test Company',
+            employer_id='12345',
+            is_employer=True,
+            bios='Test employer bio'
+        )
+
+        self.factory = RequestFactory()
+        self.upload = Upload.objects.create(
+            title='Test_Title',
+            subTitle='Test subTitle',
+            body='Test body',
+            slug='Test_Title',
+            owner=self.employer,   
+        )
+    #Test that the Upload_list view returns a status code of 200 for a GET request
+    def test_get_request(self):
+        request = self.factory.get(reverse('list'))
+        response = Upload_list(request)
+        self.assertEqual(response.status_code, 200)
+
+    #Test that the Upload_list view returns the correct template for a GET request.
+    def test_template_used(self):
+        request = self.factory.get(reverse('list'))
+        response = Upload_list(request)
+        self.assertEqual(request.path, reverse('list'))
+
+
+class ApplyCvTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',password='password')
+        self.employer = Employer.objects.create(user=self.user)
+        self.job = Upload.objects.create(title='Job Title', owner=self.employer, slug='unique-slug')
+        self.candidate = Candidate.objects.create(user=self.user, candidate_id='123', email='test@email.com',username='testuser',date_of_birth='1996-10-10')
+
+    def test_apply_cv(self):
+        response = self.client.get(reverse('ApllyCv', args=[self.job.slug]))
+        self.assertEqual(response.status_code, 404)
+        self.candidate.refresh_from_db()        
--- payments/tests.py
+++ payments/tests.py
@@ -1,3 +1,20 @@
+from django.urls import reverse
+from django.contrib.auth.models import User, Group
 from django.test import TestCase
 
-# Create your tests here.
+class AddUserToVipGroupTest(TestCase):
+    def test_add_user_to_vip_group(self):
+        group = Group.objects.create(name='VIP')
+        user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('add_user_to_vip_group'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(user.groups.filter(name='VIP').exists())
+
+    def test_add_user_to_vip_group(self):
+        group = Group.objects.create(name='VIP')
+        user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        #response = self.client.post(reverse('paymentpage'))
+        #self.assertEqual(response.status_code, 200)
+        #self.assertTrue(user.groups.filter(name='VIP').exists())
